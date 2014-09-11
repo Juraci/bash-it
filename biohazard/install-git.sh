5fa6db4d065641498b83dd03e1896b9d13b3f075
Juraci <juraci.vieira@gmail.com>
2014-09-11 17:37:04 -0300

    Improving git script

--- biohazard/install-git.sh
+++ biohazard/install-git.sh
@@ -7,6 +7,15 @@ assertions.verify_command apt-get -y install git
 assertions.verify_command git --version
 assertions.verify_command git config --global user.name "Sample Name"
 assertions.verify_command git config --global user.email sample@email.com
+
+assertions.directory_exists? ~/.ssh
+if [ $? -ne 0 ]; then
+  assertions.verify_command mkdir ~/.ssh
+fi
+
+# program that reads the contents of a file to the clipboard
+assertions.verify_command apt-get -y install xclip
+
 echo "Follow this steps manually to link your git with your github account:"
 echo "SSH configuration https://help.github.com/articles/generating-ssh-keys"
 echo "or HTTPS configuration https://help.github.com/articles/caching-your-github-password-in-git"
