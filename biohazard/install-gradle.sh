cb90ca8ac6e18ffcb136fbec6e5d3febd498e493
Juraci <juraci.vieira@gmail.com>
2014-09-26 15:59:37 -0300

    Adding gradle installation script

---
+++ biohazard/install-gradle.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+# What: Gradle installation
+# How to: http://askubuntu.com/questions/328178/gradle-in-ubuntu
+# Version tested: Ubuntu 14.04 LTS
+dir=${0%/*}
+source $dir/assertions.sh
+
+logger.log_it "---------- Gradle installation ----------"
+
+logger.log_it "[requirements test]"
+
+assertions.verify_command java -version
+java=$?
+
+assertions.verify_command "dpkg --list | grep '(JDK) [6-9]'"
+jdk_version=$?
+
+if [[ $java -ne 0 || $jdk_version -ne 0 ]]; then
+  logger.log_it "requirements not met, aborting..."
+  exit 1
+fi
+
+assertions.verify_command add-apt-repository -y ppa:cwchien/gradle
+assertions.verify_command apt-get -y update
+assertions.verify_command apt-get install gradle
+assertions.verify_command gradle --version
--- biohazard/main-scrip.sh
+++ biohazard/main-scrip.sh
@@ -5,4 +5,5 @@ dir=${0%/*}
 source $dir/post-install-procedures.sh
 source $dir/install-vim.sh
 source $dir/install-java.sh
+source $dir/install-gradle.sh
 source $dir/install-git.sh
